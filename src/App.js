632aa9cc79a54ab9e1c7ae6ad43908490354a55a
Charlotte Shih <shih.charlotte@gmail.com>
2020-02-07 14:09:12 -0800

    Updated dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 function App() {
+
+  const helloWorld = 'Hello, World!';
+  console.log(helloWorld);
+
   return (
     <main className='App'>
       {/* content goes here */}
+      woohoo i'm an app!
     </main>
   );
 }
